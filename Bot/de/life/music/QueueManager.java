78ec316508c48c1bb7f29576ceb2aa075be56f32
lpotocnik <linus.potocnik@gmail.com>
2021-04-19 19:40:58 +0200

    Musik gefixed insha'allah

---
+++ Bot/de/life/music/QueueManager.java
@@ -0,0 +1,26 @@
+package de.life.music;
+
+import java.util.HashMap;
+
+import net.dv8tion.jda.api.entities.Guild;
+
+public class QueueManager {
+
+	private static QueueManager INSTANCE;
+	private HashMap<Long, Queue> queueMap;
+	
+	public QueueManager() {
+		queueMap = new HashMap<Long, Queue>();
+	}
+
+	public static QueueManager getInstance() {
+		if (INSTANCE == null)
+			INSTANCE = new QueueManager();
+		return INSTANCE;
+	}
+
+	public Queue getQueue(Guild guild) {
+		return this.queueMap.putIfAbsent(guild.getIdLong(), new Queue());
+	}
+
+}
