1833ce91cde410bb58b817c10e69860efa717dad
lpotocnik <linus.potocnik@gmail.com>
2021-06-30 17:29:36 +0200

    Report-Command fertiggestellt

--- Bot/de/life/classes/slashexecutors/Report.java
+++ Bot/de/life/classes/slashexecutors/Report.java
@@ -1,67 +1,78 @@
 package de.life.classes.slashexecutors;
 
-import java.awt.Color;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
-
-import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+import java.util.ArrayList;
+import java.util.Collections;
 
 import de.life.classes.CategoryFinder;
-import net.dv8tion.jda.api.EmbedBuilder;
+
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.Message.Attachment;
 import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.interactions.components.ActionRow;
 import net.dv8tion.jda.api.interactions.components.Button;
-import net.dv8tion.jda.api.interactions.components.ButtonStyle;
 
 public class Report {
 
 	public Report(User reporter, User target, Guild guild, MessageChannel channel, String reason) {
-		Long channelID = CategoryFinder.getReportCategory(guild.getIdLong()).createTextChannel(target.getAsTag())
-				.complete().getIdLong();
-		MessageChannel reportChannel = guild.getTextChannelById(channelID);
-		List<Message> messageHistory = channel.getIterableHistory().complete();
-		EmbedBuilder builder = new EmbedBuilder().setTitle(target.getAsTag() + " - #" + channel.getName());
+		MessageChannel sameGuildChannel = guild.getTextChannelById(CategoryFinder.getReportCategory(guild.getIdLong())
+				.createTextChannel(target.getAsTag()).complete().getIdLong());
 
-		Integer i = 0;
-		Integer ii = 0;
-		for (Message curr : messageHistory) {
-			if (curr.getAuthor().equals(target)) {
-				i++;
-				builder.addField(curr.getTimeCreated().format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss")),
-						(curr.getContentDisplay().length() > 1014
-								? curr.getContentDisplay().substring(0, 1014) + " **[...]**"
-								: curr.getContentDisplay()),
-						true);
-				builder.addBlankField(true);
-				builder.addBlankField(true);
-			}
-			ii++;
-			if (ii > 200)
+		ArrayList<String> messageHistoryList = new ArrayList<String>();
+		int i = 0;
+		String historyString = "";
+		String url = "";
+
+		for (Message m : channel.getIterableHistory()) {
+			if (i > 49)
 				break;
+			if (!m.getAuthor().isBot()) {
+				String current = m.getAuthor().getAsTag() + " - "
+						+ m.getTimeCreated().format(DateTimeFormatter.ofPattern("dd.MM.yyyy - HH:mm"));
+
+				if (m.getAttachments().size() > 0) {
+					current = current.concat(" - Nachricht mit Anhang - Link(s):");
+					for (Attachment attachment : m.getAttachments()) {
+						current = current.concat(" " + attachment.getUrl());
+					}
+				}
+				current = current.concat("\n" + m.getContentDisplay() + "\n\n");
+				messageHistoryList.add(current);
+				i++;
+			}
+		}
+		Collections.reverse(messageHistoryList);
+
+		for (String s : messageHistoryList) {
+			historyString = historyString.concat(s);
 		}
 
-		builder.setColor(Color.RED);
-		if (i == 0)
-			builder.setDescription(target.getAsTag()
-					+ " entweder in den letzten zwei Wochen oder den letzten 200 Nachrichten dieses Channels keine Nachricht geschickt");
-		builder.setFooter("Hier findest du die letzten Nachrichten von " + target.getName()
-				+ ". Bitte beachte, dass dies keine zum Zeitpunkt des Reports gelöschten Nachrichten beinhaltet.");
+		try {
+			File chatlog = File.createTempFile("chatlog", ".txt");
+			BufferedWriter bw = new BufferedWriter(new FileWriter(chatlog));
+			bw.write(historyString);
+			bw.close();
+			url = guild.getJDA().getGuildById(672454909290872852l).getTextChannelById(859446945470087178l)
+					.sendFile(chatlog).complete().getAttachments().get(0).getUrl();
+		} catch (IOException e) {
+		}
 
-		reportChannel
+		sameGuildChannel
 				.sendMessage(reporter.getAsMention() + " hat " + target.getAsMention() + " in "
-						+ guild.getTextChannelById(channel.getIdLong()).getAsMention() + " reportet.\n")
-				.complete().getIdLong();
-		reportChannel.sendMessageEmbeds(builder.build()).queue();
-		reportChannel.sendMessage("Deine Aktionen:")
-				.setActionRows(ActionRow.of(Button.success("cancel", "Report stoppen"),
-						Button.danger("warning", "Die Person verwarnen!"),
-						Button.danger("pun-reporter", "Falscher Report, Reporter verwarnen!"),
-						Button.link("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "Chatlog")))
-				.queue();
-		reportChannel.sendMessage(channel.getHistoryBefore(channel.getLatestMessageId(), 50).complete().toString())
+						+ guild.getTextChannelById(channel.getIdLong()).getAsMention() + " reportet:")
+				.setActionRows(
+						ActionRow.of(Button.link(url, "Chatlog"),
+								Button.danger("warning", "Die Person (" + target.getAsTag() + ") verwarnen!")),
+						ActionRow.of(
+								Button.danger("pun-reporter",
+										"Falscher Report, Reporter (" + reporter.getAsTag() + ") verwarnen!"),
+								Button.primary("cancel", "Report ohne Aktion schließen")))
 				.queue();
 	}
 }
